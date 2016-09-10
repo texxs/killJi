1a62227a865808a36ad8e8dd4c677c9348b66833
Texx Smith <texx@texxsmith.com>
2016-09-10 10:59:55 -0400

    name change
    changed the file name too.

--- killJi.php
+++ killJi.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
-plugin Name:  killJoi
+plugin Name:  killJi
 Description:  Kill the Emoji with this plugin that removes all the extra scritping and code related to the new emojis that are included on every page of WordPress.    
-Plugin Url:  https://texxsmith.net/killJoi
+Plugin Url:  https://texxsmith.net/killJi
 Version: 0.1
 Plugin Author:  Texx Smith
 Author URL: http://texxsmith.com
